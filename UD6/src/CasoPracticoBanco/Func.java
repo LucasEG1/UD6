f379c4394f2f2b4f01d0f9e223d53bc517e4b4ce
casa <casa@Lucardio>
2022-01-30 03:15:13 +0100

    Acabado CP Banco

--- UD6/src/CasoPracticoBanco/Func.java
+++ UD6/src/CasoPracticoBanco/Func.java
@@ -19,4 +19,33 @@ public class Func {
 
         return valor;
     }
+    public static int menu(){
+        System.out.println("1. Ver cuentas");
+        System.out.println("2. Ingresar dinero");
+        System.out.println("3. Retirar dinero");
+        System.out.println("4. Transferir dinero");
+        System.out.println("5. Agregar cuenta");
+        System.out.println("6. Eliminar cuenta");
+        System.out.println("7. Buscar cuenta");
+        System.out.println("8. Mostrar morosos");
+        System.out.println("9. Salir");
+        
+        int opcion = rango(1,9);
+        return opcion;
+    }
+    public static Cuenta crearCuenta(String nombre, double saldo){
+        Cuenta c = new Cuenta(nombre, saldo);
+        return c;
+    }
+    public static String pedirString(){
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        return s;
+    }
+    public static double pedirDouble(){
+        Scanner leer = new Scanner(System.in);
+        System.out.print("\nCantidad a ingresar: ");
+        double d = leer.nextDouble();
+        return d;
+    }
 }
