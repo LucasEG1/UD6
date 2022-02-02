8d3e4f6c5d1800bd46df3e103a605b1df8fd225e
a023325963f <a023325963f@S09-PC19>
2022-02-02 17:29:09 +0100

    Peliculas retocado

--- UD6/src/EntregableUD6/Pelicula.java
+++ UD6/src/EntregableUD6/Pelicula.java
@@ -11,4 +11,51 @@ public class Pelicula {
     private Usuarios poseedor;
 
     private boolean esAlquilada;
+
+    public int cuantasPelis = 0;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(String fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public Usuarios getPoseedor() {
+        return poseedor;
+    }
+
+    public void setPoseedor(Usuarios poseedor) {
+        this.poseedor = poseedor;
+    }
+
+    public boolean isEsAlquilada() {
+        return esAlquilada;
+    }
+
+    public void setEsAlquilada(boolean esAlquilada) {
+        this.esAlquilada = esAlquilada;
+    }
+
+    public void mostrarPelicula() {
+        System.out.println("Título: " + titulo + "\nEstrenada en " + fechaEstreno + " trata de: " + sinopsis + " y esta alquilada por " + poseedor); //getNombre        
+    }
+
 }
